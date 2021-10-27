4ed0cf3c53fac0c410b1b9b245e85537bd2d8979
Bibote <90218184+Bibote@users.noreply.github.com>
2021-10-27 12:07:23 +0200

    Add files via upload

---
+++ src/factory/Main.java
@@ -0,0 +1,12 @@
+package factory;
+
+import domain.Covid19Pacient;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Covid19Pacient p1=new Covid19Pacient("aitor", 35);
+		new PacientSymptomGUI(p1);
+	}
+
+}
---
+++ src/observer/Main.java
@@ -0,0 +1,19 @@
+package observer;
+
+import java.util.Observable;
+
+import domain.Covid19Pacient;
+
+public class Main {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+//		Observable pacient=new Covid19Pacient("aitor", 35);
+//		new PacientObserverGUI (pacient);
+//		new PacientSymptomGUI (pacient);
+	}
+
+
+}
